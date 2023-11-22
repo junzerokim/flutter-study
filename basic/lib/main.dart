6b0aa42e9a0e0c72ca732a493916a599a0ee1d10
junzerokim <junzerokim@gmail.com>
2023-11-22 22:55:20 +0900

    :dizzy: Add: Simple Counter App

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,25 +1,18 @@
-import 'package:basic/pages/first_page.dart';
-import 'package:basic/pages/home_page.dart';
-import 'package:basic/pages/settings_page.dart';
+import 'package:basic/pages/counter_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/homepage': (context) => HomePage(),
-        '/settingspage': (context) => SettingsPage(),
-      },
+      home: CounterPage(),
     );
   }
 }
