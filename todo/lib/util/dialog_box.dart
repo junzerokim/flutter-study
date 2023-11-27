2213ada348e1efae3229e7e65fc1a618decada6c
junzerokim <junzerokim@gmail.com>
2023-11-27 20:34:04 +0900

    :dizzy: Add: ignore

--- todo/lib/util/dialog_box.dart
+++ todo/lib/util/dialog_box.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo/util/my_button.dart';
 
+// ignore: must_be_immutable
 class DialogBox extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
   final controller;
   VoidCallback onSave;
   VoidCallback onCancel;
@@ -17,6 +19,7 @@ class DialogBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.blue,
+      // ignore: sized_box_for_whitespace
       content: Container(
         height: 120,
         child: Column(
